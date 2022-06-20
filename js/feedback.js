38b2c0fd727b6b797944b3a530d7e108720c4544
bookmarking-host <94459260+bookmarking-host@users.noreply.github.com>
2022-06-20 06:23:06 -0700

    Update feedback.js

--- js/feedback.js
+++ js/feedback.js
@@ -1,38 +1,40 @@
-	function postToform() {
-		var field1 = $("#nameField").val();
-		var field2 = $("#emailField").val();
-		var field3 = $("#mobField").val();
-		var field4 = $("#cinema option:selected").text();
-		if(field1 == "") {
-			alert('Please Fill Your Name');
-			document.getElementById("nameField").focus();
-			return false;
-		}
-		if(field2 == "") {
-			alert('Please Fill Your Email');
-			document.getElementById("emailField").focus();
-			return false;
-		}
-		if(field3 == "" || field3.length > 10 || field3.length < 10) {
-			alert('Please Fill Your Mobile Number');
-			document.getElementById("mobField").focus();
-			return false;
-		}
-		$.ajax({
-			url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSf-SZZRVRfuXgDAXhBHR9SPdhrULweH0-2p7boYjTg9ZMSDcA/formResponse?",
-			data: {
-				"entry.1562541368": field1,
-				"entry.356859670": field2,
-				"entry.882151036": field3,
-				"entry.1839829419": field4
-			},
-			type: "GET",
-			dataType: "xml",
-			success: function(d) {},
-			error: function(x, y, z) {
-				$('#success-msg').show();             
-				$('#form').hide();
-			}
-		});
-		return false;
-	}
+function postToform() {
+                var field1 = $("#nameField").val();
+                var field2 = $("#emailField").val();
+                var field3 = $("#mobField").val();
+                var field4 = $("#cinema option:selected").text();
+ 				var field5 = $("#suggestion").val();
+				if(field1 == ""){
+					alert('Please Fill Your Name');
+					document.getElementById("nameField").focus();
+					return false;
+				}
+				if(field2 == ""){
+					alert('Please Fill Your Email');
+					document.getElementById("emailField").focus();
+					return false;
+				}
+				if(field3 == "" || field3.length > 10 || field3.length < 10){
+					alert('Please Fill Your Mobile Number');
+					document.getElementById("mobField").focus();
+					return false;
+				}
+
+                $.ajax({
+                    url: "https://docs.google.com/forms/d/e/1FAIpQLSerqGc4IgpisUZXEmjVloJEI3e_rRdTEP2njTpwwt38udiK8A/formResponse?",
+data: {"entry.2068933832": field1, "entry.829486952": field2, "entry.365399180": field3, "entry.1136261082":field4,"entry.524798750": field5},
+                    type: "GET",
+                    dataType: "xml",
+                    success: function(d)
+					{
+					},
+					error: function(x, y, z)
+						{
+
+							$('#success-msg').show();
+							$('#form').hide();
+							
+						}
+                });
+				return false;
+            }
